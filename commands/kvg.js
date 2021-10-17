0ef8491d7c1bf406b23ff294425c8a4ac12eaa25
Eetu Siponen <eetu.siponen@gmail.com>
2021-10-17 11:08:19 +0300

    clearing

--- commands/kvg.js
+++ commands/kvg.js
@@ -3,10 +3,6 @@ module.exports = {
     description: "Kato Vittu Googlesta",
     execute(message, args){
 
-        // if(message.member.roles.cache.has('898847944944549888')){
             message.channel.send('KVG https://www.google.com');
-        // } else {
-        //     message.channel.send(message.member.toString() + ' You dont have permission to use this command');
-        // }
     }
 }
--- commands/rolecheck.js
+++ commands/rolecheck.js
@@ -2,9 +2,10 @@ module.exports = {
     name: 'rolecheck',
     description: "Checks if you have role",
     execute(message, args){
-
+        //Gets the id of role named Mod from discord server (guild) that the message was sent to
         let role = message.guild.roles.cache.find(r => r.name === "Mod");
 
+        //Checks if the sender of that message has that role if not it gives it 
         if(message.member.roles.cache.some(r => r.name === "Mod")){
             message.channel.send('You have a Mod role already');
         } else {
